43e9d1f119f5e6343527b6068fd52eb24162a1c8
natefoster61 <nfost003@gmail.com>
2026-05-13 13:19:01 -0400

    Update config.js

--- config.js
+++ config.js
@@ -1,2 +1,2 @@
 export const SUPABASE_URL     = 'https://ryznewwwoevlwoysttcg.supabase.co';
-export const SUPABASE_ANON_KEY = 'sb_secret_UzMHhJ1VFAPV-QHxFzqIDg_4nk6KIfo';
+export const SUPABASE_ANON_KEY = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InJ5em5ld3d3b2V2bHdveXN0dGNnIiwicm9sZSI6ImFub24iLCJpYXQiOjE3Nzg2NTQyMzQsImV4cCI6MjA5NDIzMDIzNH0.oo6a-dICOvu2j-BPwQ3qnBq3_7tlmeqp1X6gKYN-Vaw';
